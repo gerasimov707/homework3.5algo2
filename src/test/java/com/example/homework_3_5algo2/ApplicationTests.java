75a72c71912527fc8cbe51be0b4e491ab54cfc2d
gerasimov707 <gerasimov707@yandex.ru>
2022-07-26 18:29:50 +0300

    коммит

---
+++ src/test/java/com/example/homework_3_5algo2/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework_3_5algo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
